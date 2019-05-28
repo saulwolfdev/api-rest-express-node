4d7535bbae10b8bb653ae384737aaa647a03220a
saul <43302778+saulwolfcode@users.noreply.github.com>
2019-05-28 20:36:01 -0300

    MIDDLEWARES

--- index.js
+++ index.js
@@ -5,9 +5,39 @@ const bodyParser=require("body-parser")
 const express = require("express")
 const app =express()
 
+//middlewares
+function logger(req,res,next){
+    console.log(`Rouwtes:=> ${req.protocol}://${req.get("host")}${req.originalUrl}`)
+    next()
+}
+app.use(express.json())
+app.use(logger)
+
 app.get("/",(req,res)=>{
-    res.send("Server OK")
+    res.send("GET")
 })
-app.listen(3000,()=>{
-    console.log("Server on port 3000 =>")
+app.get("/User",(req,res)=>{
+    console.log("with get")
+    res.json({
+        username:"Saul",
+        lastname:"Alejos"
+    })
+})
+app.use(express.json())
+app.post("/User",(req,res)=>{
+    console.log("with post")
+    console.log(req.body)
+    res.send("POST OK")
+})
+app.post("/About",(req,res)=>{
+    res.send("POST")
+})
+app.put("/Home",(req,res)=>{
+    res.send("PUT")
+})
+app.delete("/Contact",(req,res)=>{
+    res.send("DELETE")
+})
+app.listen(5000,()=>{
+    console.log("Server on port 5000 =>")
 })
