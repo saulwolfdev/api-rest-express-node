1cb30c015566d447d75100840f9f0af63a49fce6
saul <43302778+saulwolfcode@users.noreply.github.com>
2019-05-28 21:25:24 -0300

    EJS

--- index.js
+++ index.js
@@ -1,22 +1,32 @@
-"use strict";
 const morgan=require("morgan")
 const bodyParser=require("body-parser")
 
 const express = require("express")
 const app =express()
 
+//Settings
+
+app.set("AppName","Wolf")
+app.set("port",3000)
+app.set("view engine", "ejs")
+
 //middlewares
-function logger(req,res,next){
-    console.log(`Rouwtes:=> ${req.protocol}://${req.get("host")}${req.originalUrl}`)
-    next()
-}
+// function logger(req,res,next){
+//     console.log(`Rouwtes:=> ${req.protocol}://${req.get("host")}${req.originalUrl}`)
+//     next()
+// }
 app.use(express.json())
 // app.use(logger)
 app.use(morgan("dev"))
 
+// app.get("/",(req,res)=>{
+//     res.send("GET")
+// })
 app.get("/",(req,res)=>{
-    res.send("GET")
+   const data=[{name:"Saul"},{name:"Sama"},{name:"Saint"}]
+    res.render("index.ejs",{people:data})
 })
+
 app.get("/User",(req,res)=>{
     console.log("with get")
     res.json({
@@ -39,6 +49,10 @@ app.put("/Home",(req,res)=>{
 app.delete("/Contact",(req,res)=>{
     res.send("DELETE")
 })
-app.listen(5000,()=>{
+
+app.use(express.static("public"))
+
+app.listen(app.get("port"),()=>{
+    console.log(app.get("AppName"))
     console.log("Server on port 5000 =>")
 })
