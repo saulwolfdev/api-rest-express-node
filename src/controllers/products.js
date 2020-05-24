36a7fe3aa8a0e2fa71e0a8087174c4b0424a6801
saul alejos garay <43302778+saulwolfdev@users.noreply.github.com>
2020-05-24 00:21:45 -0300

    put, delete

--- src/controllers/products.js
+++ src/controllers/products.js
@@ -8,5 +8,32 @@ module.exports={
 		const {name}=req.body;
 		products.push({id:products.length,name});
 		res.json({"request":"this product was charge"});
+	},
+	updateProducts:(req,res)=>{
+		const {id}=req.params;
+		const {name}=req.body;
+		products.forEach((product,i)=>{
+			if(product.id===Number(id)){
+				product.name=name;
+			}
+		});
+		res.json({
+			"succes":true,
+			"messages":"updates"
+
+		});
+	},
+	deleteProducts:(req,res)=>{
+		const {id}=req.params;
+		products.forEach((product, i)=>{
+			if(product.id===Number(id)){
+				products.splice(i,1);
+			}
+		});
+		res.json({
+			"success":true,
+			"messages":"delete"
+		});
+		
 	}
 };
--- src/routes/products.js
+++ src/routes/products.js
@@ -1,16 +1,23 @@
 
-const express=require("express");
-const router=express.Router();
+const express = require("express");
+const router = express.Router();
 
 
 
 //CONTROLLERS
-const {getProducts,addProducts} =require("../controllers/products");
+const {
+	getProducts,
+	addProducts,
+	updateProducts,
+	deleteProducts
+} = require("../controllers/products");
 router.route("/")
 	.get(getProducts)
 	.post(addProducts);
+router.route("/:id")
+	.put(updateProducts)
+	.delete(deleteProducts);
 
 
 
-
-module.exports=router;
+module.exports = router;
